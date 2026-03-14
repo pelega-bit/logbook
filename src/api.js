bd28f6f74721fb055c4a021cee35aa06cf2223e4
Peleg Avigdor <peleg@skanarobotics.com>
2026-03-15 01:42:31 +0200

    first commit

---
+++ src/api.js
@@ -0,0 +1,17 @@
+const API_URL = "https://script.google.com/a/macros/skanarobotics.com/s/AKfycbw5fsb8QE5wnL6plSRIF8U9Q8roUZ00y6err07_T1zzltNcDLgqB8XNMrT9c1oWhHk3Pg/exec";
+
+export async function fetchSheet(sheet) {
+  const res = await fetch(`${API_URL}?sheet=${sheet}`);
+  return res.json();
+}
+
+export async function addRow(sheet, data) {
+  const res = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ sheet, data }),
+  });
+  return res.json();
+}
